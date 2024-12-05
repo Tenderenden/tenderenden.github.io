0d742cfb54d2bb7bcfcbcec41b24f6b7f34767b5
Tomasz Tendera <tomektendera@gmail.com>
2024-12-05 19:33:49 +0100

    Try fixing site generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ import vercel from '@astrojs/vercel/serverless'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://tenderenden.github.io/',
-	repo: 'tenderenden.github.io',
+	// repo: 'tenderenden.github.io',
 	integrations: [
 		expressiveCode(expressiveCodeOptions),
 		tailwind({
